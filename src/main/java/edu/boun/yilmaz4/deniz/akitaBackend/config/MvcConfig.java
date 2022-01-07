cc747987d6c041e2743e8bb443edf44a392a0a72
lovemrlemon <yilmaz.zeynepdeniz@gmail.com>
2022-01-07 14:05:47 +0300

    delete unnecessary imports

--- src/main/java/edu/boun/yilmaz4/deniz/akitaBackend/config/MvcConfig.java
+++ src/main/java/edu/boun/yilmaz4/deniz/akitaBackend/config/MvcConfig.java
@@ -1,16 +1,8 @@
 package edu.boun.yilmaz4.deniz.akitaBackend.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Description;
-import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.thymeleaf.spring5.SpringTemplateEngine;
-import org.thymeleaf.spring5.view.ThymeleafViewResolver;
-import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
-import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
-
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
